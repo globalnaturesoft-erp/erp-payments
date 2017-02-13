fc9733734bc2c75ad06f7a9b2f2c07186ea4f3bd
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-12 19:23:10 -0800

    display order information

--- app/models/erp/payments/payment_record.rb
+++ app/models/erp/payments/payment_record.rb
@@ -29,5 +29,69 @@ module Erp::Payments
       accountant.present? ? accountant.name : ''
     end
     
+    # DISPLAY ORDER INFORMATION
+    # order date
+    def order_date(params)
+      if order.present?
+        order.order_date
+      elsif params[:order_id].present?
+        Erp::Sales::Order.find(params[:order_id]).order_date
+      end
+    end
+    
+    # order expiration date
+    def expiration_date(params)
+      if order.present?
+        order.expiration_date
+      elsif params[:order_id].present?
+        Erp::Sales::Order.find(params[:order_id]).expiration_date
+      end
+    end
+    
+    # order code
+    def order_code(params)
+      if order.present?
+        order.code
+      elsif params[:order_id].present?
+        Erp::Sales::Order.find(params[:order_id]).code
+      end
+    end
+    
+    # order customer name
+    def order_customer(params)
+      if order.present?
+        order.customer_name
+      elsif params[:order_id].present?
+        Erp::Sales::Order.find(params[:order_id]).customer_name
+      end
+    end
+    
+    # order paid amount
+    def order_paid_amount(params)
+      if order.present?
+        order.paid_amount
+      elsif params[:order_id].present?
+        Erp::Sales::Order.find(params[:order_id]).paid_amount
+      end
+    end
+    
+    # order total
+    def order_total(params)
+      if order.present?
+        order.total
+      elsif params[:order_id].present?
+        Erp::Sales::Order.find(params[:order_id]).total
+      end
+    end
+    
+    # order remain amount
+    def order_remain_amount(params)
+      if order.present?
+        order.remain_amount
+      elsif params[:order_id].present?
+        Erp::Sales::Order.find(params[:order_id]).remain_amount
+      end
+    end
+    
   end
 end
