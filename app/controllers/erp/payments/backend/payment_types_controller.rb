2125791d5296784b7cf1531a3f4146c70f089a1d
Luan Pham <luanpm88@gmail.com>
2017-12-13 08:35:36 +0700

    update from server

--- app/controllers/erp/payments/backend/payment_types_controller.rb
+++ app/controllers/erp/payments/backend/payment_types_controller.rb
@@ -3,27 +3,27 @@ module Erp
     module Backend
       class PaymentTypesController < Erp::Backend::BackendController
         before_action :set_payment_type, only: [:edit, :update, :set_active, :set_deleted]
-        
+
         # POST /payment_types/list
         def list
           @payment_types = PaymentType.search(params).paginate(:page => params[:page], :per_page => 10)
-          
+
           render layout: nil
         end
-    
+
         # GET /payment_types/new
         def new
           @payment_type = PaymentType.new
         end
-    
+
         # GET /payment_types/1/edit
         def edit
         end
-    
+
         # POST /payment_types
         def create
           @payment_type = PaymentType.new(payment_type_params)
-          
+
           if @payment_type.save
             @payment_type.set_code_is_custom
             @payment_type.set_active
@@ -40,18 +40,18 @@ module Erp
             render :new
           end
         end
-    
+
         # PATCH/PUT /payment_types/1
         def update
           if @payment_type.update(payment_type_params)
             @payment_type.set_code_is_custom
-            
+
             if request.xhr?
               render json: {
                 status: 'success',
                 text: @payment_type.name,
                 value: @payment_type.id
-              }              
+              }
             else
               redirect_to erp_payments.backend_payment_types_path, notice: t('.success')
             end
@@ -59,7 +59,7 @@ module Erp
             render :edit
           end
         end
-        
+
         # ACTIVE /payment_records/1
         def set_active
           @payment_type.set_active
@@ -73,7 +73,7 @@ module Erp
             }
           end
         end
-        
+
         # DELETED /payment_records/1
         def set_deleted
           @payment_type.set_deleted
@@ -87,7 +87,7 @@ module Erp
             }
           end
         end
-        
+
         def dataselect
           respond_to do |format|
             format.json {
@@ -95,13 +95,13 @@ module Erp
             }
           end
         end
-    
+
         private
           # Use callbacks to share common setup or constraints between actions.
           def set_payment_type
             @payment_type = PaymentType.find(params[:id])
           end
-    
+
           # Only allow a trusted parameter "white list" through.
           def payment_type_params
             params.fetch(:payment_type, {}).permit(:name, :is_payable, :is_receivable, :status)
